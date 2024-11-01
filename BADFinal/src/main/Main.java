359b4b17c20f0eb156e00a97dd273e0292b7b41b
DaveEugeneWijaya <145684839+DaveEugeneWijaya@users.noreply.github.com>
2024-11-01 17:41:43 +0700

    Ganti anjing jadi kucing

--- BADFinal/src/main/Main.java
+++ BADFinal/src/main/Main.java
@@ -219,7 +219,7 @@ public class Main extends Application{
 		arrangeComponentLogin();
 		
 		// Start Scene
-		stage.setTitle("On Boarding Page anjingggggg");
+		stage.setTitle("On Boarding Page kucinggg");
 		stage.setResizable(false);
 		stage.setScene(sceneOnBoarding);
 		stage.show();
